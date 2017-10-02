a399023adaae5fcbb38db6447fe9be438c5df203
Neville Bonavia <nbonavia@gmail.com>
2017-10-02 05:59:54 +0200

    adding e-mail validation

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -4,6 +4,7 @@ import { Form, Text } from 'react-form'
 
 class LoginForm extends Component {
     render() {
+        var emailRegex=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return (
             <div className=".col-12">
                 <div className=".col-4">
@@ -11,11 +12,11 @@ class LoginForm extends Component {
                     onSubmit={(values) => {
                     console.log('Success!', values)
                     }}
-                    validate={({ name, password }) => {
-                    return {
-                        password: (!password || password.length < 6) ? 'A password of 6 or more characters is required' : null
-
-                    }
+                    validate={({ email, password }) => {
+                        return {
+                            password: (!password || password.length < 6) ? 'A password of 6 or more characters is required' : null,
+                            email: (!emailRegex.test(email)) ? 'Invalid e-mail': null
+                        }
                     }}
                 >
                     {({submitForm}) => {
