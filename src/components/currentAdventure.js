11a7c4b753048546e2a669918cf3a03d82ca47e6
nebo <neville.bonavia@betssongroup.com>
2017-11-25 19:56:47 +0100

    Successfully backing up a project connected together

--- src/components/currentAdventure.js
+++ src/components/currentAdventure.js
@@ -30,56 +30,62 @@ export class CurrentAdventure extends React.Component {
 
   async updateAdventureDetails() {
     let adventure = await http.get('adventures/' + this.state.projectTitle + '/');
-    let joinedRewards=null;
+    let joinedReward=null;
     try {
-      joinedRewards = await http.get('join/rewards/' + adventure._id);
+      joinedReward = await http.get('join/rewards/' + adventure._id);
     } catch (err) {
       console.log('The user might not be logged in');
     }
-    debugger;
     console.log(adventure)
-    this.setState({adventure:adventure, joinedRewards: joinedRewards});
+    this.setState({adventure:adventure, joinedReward: joinedReward});
   }
 
   async onButtonClick(event) {
-    // let projectName = event.target.getAttribute('data-project')
     let adventureId = event.target.getAttribute('data-adventureID');
     let rewardId = event.target.getAttribute('data-rewardID');
-    // hashHistory.push('/view/' + projectName)
 
-    //Join the adventure - REST API Call (join/create)
     await http.post('join/create', {adventureID: adventureId, rewardID: rewardId});
     this.updateAdventureDetails();
   }
 
   
-    render() {
+  render() {
       console.log(this.state.adventure.text);
+      let adventureId = this.state.adventure._id;
+      let self = this;
+
       const rewardDetails = this.state.adventure.rewards.map((item, index) => {
         let rewardLevel = item.amount;
         let reward = rewardLevel.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-          if(true) {
-          return (
-            <div className="rewards" key={index}>
-              <div className="row rewardContainer" >
-                  <div>
-                    <p className="individual_title">{item.title}</p>
-                    <p className="individual_amount"><strong>${reward}</strong></p>
-                    <p className="individual_description">{item.descripton}</p>
-                  </div>
 
-                  <div>                  
-                   <button className="button_adventure" onClick={this.onButtonClick}>Join this Adventure</button>
-                  </div>
-
-              </div>
-            </div>
-          )
+        let joinButton = null;
+        if (self.state.joinedReward == null) {
+          joinButton = <div>
+                        <button className="button_adventure" data-adventureID={adventureId} data-rewardID={item._id} onClick={this.onButtonClick}>Join this Adventure</button>
+                      </div>;
         } 
+
+        let backedClassName = "";
+        if (self.state.joinedReward != null && self.state.joinedReward.rewardID == item._id) {
+          backedClassName="backed";
+        }
+
+        return (
+          <div className="rewards" key={index}>
+            <div className={backedClassName + " row rewardContainer"}>
+                <div>
+                  <p className="individual_title">{item.title}</p>
+                  <p className="individual_amount"><strong>${reward}</strong></p>
+                  <p className="individual_description">{item.descripton}</p>
+                </div>
+
+                {joinButton}
+            </div>
+          </div>
+        );
       })
  
       return(
-       
         <div className="individual">
             <div className="row">
               <div className="col-12">
